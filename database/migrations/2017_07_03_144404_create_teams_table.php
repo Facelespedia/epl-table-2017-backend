52fb2a085910713d2ef4723d15a7a38dec9bb453
Narongsak <narongsak.chobsri@gmail.com>
2017-07-03 21:49:42 +0700

    Add database table teams

---
+++ database/migrations/2017_07_03_144404_create_teams_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('club');
+            $table->integer('played');
+            $table->integer('won');
+            $table->integer('drawn');
+            $table->integer('lost');
+            $table->integer('goal_for');
+            $table->integer('goal_against');
+            $table->integer('goal_difference');
+            $table->integer('points');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('teams');
+    }
+}
