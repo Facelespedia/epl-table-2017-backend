aabbb0f56036718ddd5eaae10010837db3a6cf98
Narongsak <narongsak.chobsri@gmail.com>
2017-07-04 23:47:15 +0700

    Create seeder match month 8 day 12

--- database/seeds/ScheduleTableSeeder.php
+++ database/seeds/ScheduleTableSeeder.php
@@ -11,13 +11,26 @@ class ScheduleTableSeeder extends Seeder
      */
     public function run()
     {
+        $schedules = array();
         $date = new DateTime();
-        $date->setDate(2017, 8, 12);
+        $date_month_8 = [ [8, 12], [8, 19], [8, 26] ];
+        $match_month_8 = [[   [19, 2], [18, 10], [14, 16], 
+                               [13, 17], [12, 20], [7, 15],
+                               [6, 8], [5, 4], [3, 11], [1, 9]     ],
+                          [],[]];
+        $date_month_9 = [ ];
         $date->setTime(21, 00);
         $this->command->info('ScheduleTableSeeder');
-        $schedule = [ 'date' => $date , 'time' => $date];
+
+        for( $i = 0; $i < count($date_month_8); $i++ ) {
+            $date->setDate(2017, $date_month_8[$i][0], $date_month_8[$i][1] );
+            for( $j = 0; $j < count($match_month_8[$i]); $j++ ) {
+                $schedule = [ 'date' => $date , 'time' => $date, 'home_team_id' => $match_month_8[$i][$j][0], 'away_team_id' => $match_month_8[$i][$j][1] ];
+                array_push($schedules, $schedule);
+            }
+        }
 
         App\Schedule::truncate();
-        App\Schedule::insert($schedule);
+        App\Schedule::insert($schedules);
     }
 }
