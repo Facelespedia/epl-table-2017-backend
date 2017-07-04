e3be3dbd6c7872bfaebdd3a66990ee3a98157691
Narongsak <narongsak.chobsri@gmail.com>
2017-07-04 18:16:31 +0700

    Add function seeder team

--- database/seeds/TeamTableSeeder.php
+++ database/seeds/TeamTableSeeder.php
@@ -13,22 +13,27 @@ class TeamTableSeeder extends Seeder
     {
         $this->command->info('TeamTableSeeder');
         $teams = array();
-        for( $i = 0 ; $i < 5 ; $i++ ) {
-             $team = [
-            'club' => 'Liverpoll',
-            'played' => '0',
-            'won' => '0',
-            'drawn' => '0',
-            'lost' => '0',
-            'goal_for' => '0',
-            'goal_against' => '0',
-            'goal_difference' => '0',
-            'points' => '0',
+        $team_name = ['Arsenal', 'Bournemouth', 'Brighton and Hove Albion', 'Burnley', 'Chelsea',
+                      'Crystal Palace', 'Everton', 'Huddersfield Town', 'Leicester City', 'Liverpool',
+                      'Manchester City', 'Manchester United', 'Newcastle United', 'Southampton', 'Stoke City',
+                      'Swansea City', 'Tottenham Hotspur', 'Watford', 'West Bromwich Albion', 'West Ham United'
+                        ];
+        for( $i = 0 ; $i < 20 ; $i++ ) {
+
+            $team = [
+                'club' => $team_name[$i],
+                'played' => '0',
+                'won' => '0',
+                'drawn' => '0',
+                'lost' => '0',
+                'goal_for' => '0',
+                'goal_against' => '0',
+                'goal_difference' => '0',
+                'points' => '0',
+                'created_at' => new DateTime()
             ];
            array_push($teams, $team);
         }
-        // $obj = json_decode (json_encode ($teams), FALSE);
-        // var_dump($teams);
         DB::statement('SET FOREIGN_KEY_CHECKS=0');
         App\Team::truncate();
         App\Team::insert($teams);
