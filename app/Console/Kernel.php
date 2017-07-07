36266fa24ced973c85fd5e5d3b2888d69378ed2b
Narongsak <narongsak.chobsri@gmail.com>
2017-07-07 21:17:32 +0700

    Generate command insert schedule'

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends ConsoleKernel
         Commands\InsertTeam::class,
         Commands\DeleteTeam::class,
         Commands\EditTeam::class,
+        Commands\InsertSchedule::class,
     ];
 
     /**
