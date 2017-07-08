d7164bba6716180b02e3b549ab36f10430ea44c7
Narongsak <narongsak.chobsri@gmail.com>
2017-07-08 23:32:31 +0700

    Create input in command schedule but bug foreign key

--- app/Console/Commands/InsertSchedule.php
+++ app/Console/Commands/InsertSchedule.php
@@ -37,6 +37,36 @@ class InsertSchedule extends Command
      */
     public function handle()
     {
-        //
+        $password = $this->secret('Please enter your password!');
+        if($password == '123456') {
+            
+            $year = $this->ask('Please fill in year');
+            $month = $this->ask('Please fill in month');
+            $day = $this->ask('Please fill in day');
+            $hour = $this->ask('Please fill in time(hour)');
+            $minute = $this->ask('Please fill in time(minute)');
+            $id_home = $this->ask('Please fill in team home id');
+            $id_away = $this->ask('Please fill in team away id');
+            $team_home = \App\Team::find($id_home)->id;
+            $team_away = \App\Team::find($id_away);
+
+            // $schedule = \App\Schedule::create(['home_team_id' => $team_home, 'away_team_id' => '2']);
+            $date = new DateTime();
+            $date->setDate($year, $month, $day);
+            $date->setTime($hour, $minute);
+          
+            
+
+            // $schedule->date = $date;
+            // $schedule->time = $date;
+            // $schedule->home_team()->save($team_home);
+            // $schedule->away_team_id()->associate($team_away->id);
+            // $schedule->save();
+
+            $this->info('Insert schedule success!!');
+        }else {
+            $this->info('Your password is incorrect!!');
+            exit;
+        }
     }
 }
