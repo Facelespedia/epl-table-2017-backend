315a2a0a976e1ded2435109b03ff6d0906bec0e6
Narongsak <narongsak.chobsri@gmail.com>
2017-07-10 20:19:27 +0700

    Add function edit schedule

--- app/Console/Commands/EditSchedule.php
+++ app/Console/Commands/EditSchedule.php
@@ -11,14 +11,14 @@ class EditSchedule extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'edit:schedule';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Command edit schedule';
 
     /**
      * Create a new command instance.
@@ -37,6 +37,34 @@ class EditSchedule extends Command
      */
     public function handle()
     {
-        //
+        $password = $this->secret('Please enter your password!');
+
+        if($password == '123456') {
+            $header = ['ID', 'Club', 'Played', 'Won', 'Drawn', 'Lost', 'GF', 'GA', 'GD', 'Points'];
+            $teams = \App\Team::select('id', 'club', 'played', 'won', 'drawn','lost', 'goal_for', 'goal_against', 'goal_difference', 'points')->get();
+            
+            $this->table($header, $teams);
+            $team_home_id = $this->ask('Please enter id team home');
+            $team_away_id = $this->ask('Please enter id team away');
+
+            
+            $year = $this->ask('Please fill in year');
+            $month = $this->ask('Please fill in month');
+            $day = $this->ask('Please fill in day');
+            $hour = $this->ask('Please fill in time(hour)');
+            $minute = $this->ask('Please fill in time(minute)');
+            $date = new \DateTime();
+            $date->setDate($year, $month, $day);
+            $date->setTime($hour, $minute);
+
+            $data = [ 'date' => $date, 'time' => $date ];
+
+            \App\Schedule::where('home_team_id', $team_home_id)
+                                        ->where('away_team_id', $team_away_id)
+                                        ->update($data);
+        }else {
+            $this->info('Your password is incorrect!!');
+            exit;
+        }
     }
 }
