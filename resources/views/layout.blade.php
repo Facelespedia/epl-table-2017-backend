a9e593b09ea749934681b92b4375dd4e4b7a3f83
Narongsak <narongsak.chobsri@gmail.com>
2017-07-17 19:33:53 +0700

    Divide layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Premier League 2017-18</title>
+</head>
+<body>
+    <header>header</header>
+    <div class="container">@yield('content')</div>
+    <footer>footer</footer>
+</body>
+</html>
---
+++ resources/views/table/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layout')
+@section('content')
+    <div>55555</div>
+@stop
