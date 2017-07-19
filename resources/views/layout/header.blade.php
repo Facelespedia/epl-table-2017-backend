25e155df718d7979f2e61d8cb12daaadd45dfee5
Narongsak <narongsak.chobsri@gmail.com>
2017-07-19 17:11:51 +0700

    Create navbar column

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,13 +1,14 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="#">Premier League</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
+      <li><a href="#">Tables</a></li>
+      <li><a href="#">MatchWeeks</a></li>
+      <li><a href="#">Clubs</a></li>
+      <li><a href="#">Players</a></li>
     </ul>
   </div>
 </nav>
