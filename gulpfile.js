47059ded3ec9ebd31e92789d20efeed55e75fc81
Narongsak <narongsak.chobsri@gmail.com>
2017-08-01 04:01:22 +0700

    Add icon sign out

--- gulpfile.js
+++ gulpfile.js
@@ -19,10 +19,11 @@ elixir.extend('remove', function(path) {
 
 
 elixir(function(mix) {
-    mix.remove([ 'public/css', 'public/images', 'public/js' ]);
+    mix.remove([ 'public/css', 'public/images', 'public/js', 'public/fonts' ]);
     mix.sass('app.scss');
     mix.copy('resources/assets/images', 'public/images');
-    mix.copy('node_modules/jquery/dist/jquery.min.js', 'resources/assets/js/')
+    mix.copy('node_modules/jquery/dist/jquery.min.js', 'resources/assets/js/');
+    mix.copy('node_modules/font-awesome/fonts/', 'public/fonts');
     mix.scripts(['config.js'], 'public/js/config.js')
     .scripts(['jquery.min.js'], 'public/js/jquery.min.js');
 });
