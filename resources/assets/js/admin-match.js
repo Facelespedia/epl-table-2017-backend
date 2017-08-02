dfce929273d38333ca63caa423c689fe1fc0adc2
Narongsak <narongsak.chobsri@gmail.com>
2017-08-02 18:58:30 +0700

    Add table match

--- resources/assets/js/admin-match.js
+++ resources/assets/js/admin-match.js
@@ -1,7 +1,14 @@
 $.ajax({
   url: '/api/schedules',
   method: 'GET'
-}).done(function(data){
-	console.log(data.length);
-	
+}).done(function(data) {
+	$.each( data, function( key, value) {
+		$('#match-table tbody').append('<tr></tr>');
+		$('#match-table tbody tr:last').append('<td class="text-center">'+value.id+'</td>');
+		$('#match-table tbody tr:last').append('<td class="text-center">'+value.date+'</td>');
+		$('#match-table tbody tr:last').append('<td class="text-center">'+value.time+'</td>');
+		$('#match-table tbody tr:last').append('<td class="text-center">'+value.home_team_id+'</td>');
+		$('#match-table tbody tr:last').append('<td class="text-center">'+value.away_team_id+'</td>');
+		// console.log(key);
+	});
 });
--- resources/views/dashboard/match/index.blade.php
+++ resources/views/dashboard/match/index.blade.php
@@ -1,7 +1,7 @@
 @extends('dashboard.layout.index')
 @section('content')
-  <div class="container-match">
-    <table class="table box">
+  <div class="container-match" style="height:100%;overflow:auto;">
+    <table id="match-table" class="table box"style="height:50%;">
       <thead class="thead-inverse">
         <tr>
           <th class="text-center">Match ID</th>
@@ -12,13 +12,6 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th class="text-center">1</th>
-          <td class="text-center">Mark</td>
-          <td class="text-center">Otto</td>
-          <td class="text-center">a</td>
-					<td class="text-center">b</td>
-        </tr>
       </tbody>
     </table>
   </div>
