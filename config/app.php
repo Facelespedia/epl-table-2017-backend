deed7164d017f6ddb92078a4bc35bb7bd5c6731c
Narongsak <narongsak.chobsri@gmail.com>
2017-08-04 02:53:12 +0700

    Modify URL

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://premier-league-2017-18.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
